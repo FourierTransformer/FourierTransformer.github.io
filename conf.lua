79570402d352b4e82c57cd2d6ba44af4b0acef79
Shakil Thakur <coolshakil@gmail.com>
2014-11-18 20:13:29 -0600

    ecto: update blog to version 10

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 local conf = {
     location = "/Users/shakil/Source/ghost-blog/",
     server = "http://localhost:2368/",
-    version = 9,
+    version = 10,
     cname = "shakilthakur.com"
 }
 return conf
